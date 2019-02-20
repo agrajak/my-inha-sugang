49a0422e7411b3f2aa0b8d3620b8b08f0aeb37e2
Agrajak <korbots@gmail.com>
2019-02-20 21:09:23 +0900

    토글 추가

--- components/SubjectViewer.vue
+++ components/SubjectViewer.vue
@@ -1,153 +1,87 @@
 <template>
   <div id="subject-viewer">
-    <section class="section">
-      <div class="container control">
-        <div class="field is-horizontal">
-          <div class="field-label is-normal">
-            <label class="label">검색필터</label>
-          </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded">
-                <input class="input" v-model="search" placeholder="과목이름/분류등을 입력해보세요">
-              </p>
-            </div>
-          </div>      
-        </div>
-        <div class="field is-horizontal">
-          <div class="field-label is-normal">
-            <label class="label">최소학점 / 최대학점</label>
-          </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded">
-                <input class="input" v-model="minCredit" placeholder="최소학점">
-              </p>
-            </div>
-            <div class="field">
-              <p class="control is-expanded">
-                <input class="input" v-model="maxCredit" placeholder="최대학점">
-              </p>
-            </div>
-          </div>      
-        </div>
-        <div class="field is-horizontal">
-          <div class="field-label is-normal">
-            <label class="label">분류</label>
-          </div>
-          <div class="field-body">
-            <div class="field">
-              <div class="field is-grouped is-grouped-centered">
-                <label class="radio">
-                  <input type="radio" v-model="category" value="전체"> 전체
-                </label>
-                <label class="radio" >
-                  <input type="radio" v-model="category" value="전공선택"> 전공선택
-                </label>
-                <label class="radio" >
-                  <input type="radio" v-model="category" value="전공필수"> 전공필수
-                </label>
-                <label class="radio" >
-                  <input type="radio" v-model="category" value="핵심교양"> 핵심교양
-                </label>
-                <label class="radio" >
-                  <input type="radio" v-model="category" value="교양선택"> 교양선택
-                </label>
-                <label class="radio" >
-                  <input type="radio" v-model="category" value="교양필수"> 교양필수
-                </label>
+    <table class="table is-narrow is-hoverable is-fullwidth">
+      <thead>
+        <th>학수번호</th>
+        <th>과목이름</th>
+        <th>학년</th>
+        <th>학점</th>
+        <th>분류</th>
+        <th>시간</th>
+        <th>장소</th>
+        <th>교수이름</th>
+        <th>평가</th>
+        <th>비고</th>
+        <th></th>
+      </thead>
+      <tbody>
+        <!-- 희망과목들 -->
+        <tr v-for="(cell, i) in desired_subject" :key="i" class="is-selected">
+          <template v-if="cell.code.indexOf('-') == -1">
+            <td>{{cell.code}}</td>
+            <td>{{cell.detail.subject}}</td>
+            <td>{{cell.detail.grade}}</td>
+            <td>{{cell.detail.credit}}</td>
+            <td>{{cell.detail.category}}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <div class="buttons">
+                <button class="button is-small is-danger" @click="$emit('delete_subject', cell.code)">해당 과목 삭제</button>
+                <button @click="$emit('toggle_subject', cell.code)" class="button is-small" :class="{'is-link': cell.important}">필수여부({{cell.important?'O':'X'}})</button>
               </div>
-            </div>
-          </div>      
-        </div>
-      </div>  
-    </section>
-    <section class="section">
-      <div class="container">
-        <table class="table is-narrow is-hoverable">
-          <thead>
-            <th>학수번호</th>
-            <th>과목이름</th>
-            <th>학년</th>
-            <th>학점</th>
-            <th>분류</th>
-            <th>시간</th>
-            <th>장소</th>
-            <th>교수이름</th>
-            <th>평가</th>
-            <th>비고</th>
-            <th></th>
-          </thead>
-          <tbody>
-            <!-- 희망과목들 -->
-            <tr v-for="(cell, i) in desired_subject" :key="i" class="is-selected">
-              <template v-if="cell.code.indexOf('-') == -1">
-                <td>{{cell.code}}</td>
-                <td>{{cell.detail.subject}}</td>
-                <td>{{cell.detail.grade}}</td>
-                <td>{{cell.detail.credit}}</td>
-                <td>{{cell.detail.category}}</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>
-                  <div class="buttons">
-                    <button class="button is-small is-danger" @click="$emit('delete_subject', cell)">해당 과목 삭제</button>
-                    <button class="button is-small is-link">필수여부(O)</button>
-                  </div>
-                </td>
-              </template>
-              <template v-else>
-                <td>{{cell.code}}</td>
-                <td>{{cell.detail.subject}}</td>
-                <td>{{cell.detail.grade}}</td>
-                <td>{{cell.detail.credit}}</td>
-                <td>{{cell.detail.category}}</td>
-                <td>{{cell.detail.time}}</td>
-                <td>{{cell.detail.place}}</td>
-                <td>{{cell.detail.name_pf}}</td>
-                <td>{{cell.detail.rate}}</td>
-                <td>{{cell.detail.bigo}}</td>
-                <td>
-                  <div class="buttons">
-                    <button class="button is-small is-danger" @click="$emit('delete_bunban', cell)">해당 분반 삭제</button>
-                    <button class="button is-small is-link">필수여부(O)</button>
-                  </div>
-                </td>
-              </template>
-            </tr>
-            <!-- 선택가능 과목들 -->
-            <tr v-for="(cell, i) in list_page" :key="i">
-              <td>{{cell.sno}}</td>
-              <td>{{cell.subject}}</td>
-              <td>{{cell.grade}}</td>
-              <td>{{cell.credit}}</td>
-              <td>{{cell.category}}</td>
-              <td>{{cell.place_detail}}</td>
-              <td>{{cell.place}}</td>
-              <td>{{cell.name_pf}}</td>
-              <td>{{cell.rate}}</td>
-              <td>{{cell.bigo}}</td>
-              <td>
-                <div class="buttons has-addons">
-                  <button class="button is-small" @click="$emit('add_subject', cell)">과목 추가</button>
-                  <button class="button is-small" @click="$emit('add_bunban', cell)">분반만 추가</button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+            </td>
+          </template>
+          <template v-else>
+            <td>{{cell.code}}</td>
+            <td>{{cell.detail.subject}}</td>
+            <td>{{cell.detail.grade}}</td>
+            <td>{{cell.detail.credit}}</td>
+            <td>{{cell.detail.category}}</td>
+            <td>{{cell.detail.time}}</td>
+            <td>{{cell.detail.place}}</td>
+            <td>{{cell.detail.name_pf}}</td>
+            <td>{{cell.detail.rate}}</td>
+            <td>{{cell.detail.bigo}}</td>
+            <td>
+              <div class="buttons">
+                <button class="button is-small is-danger" @click="$emit('delete_bunban', cell.code)">해당 분반 삭제</button>
+                <button class="button is-small is-link">필수여부(O)</button>
+              </div>
+            </td>
+          </template>
+        </tr>
+        <!-- 선택가능 과목들 -->
+        <tr v-if="list_page.length == 0">
+          <td colspan="11">조건에 해당하는 결과값이 없습니다.</td>
+        </tr>
 
-    </section>
-    
-    
-    
+        <tr v-else v-for="(cell, i) in list_page" :key="i+desired_subject.length">
+          <td>{{cell.sno}}</td>
+          <td>{{cell.subject}}</td>
+          <td>{{cell.grade}}</td>
+          <td>{{cell.credit}}</td>
+          <td>{{cell.category}}</td>
+          <td>{{cell.place_detail}}</td>
+          <td>{{cell.place}}</td>
+          <td>{{cell.name_pf}}</td>
+          <td>{{cell.rate}}</td>
+          <td>{{cell.bigo}}</td>
+          <td>
+            <div class="buttons has-addons">
+              <button class="button is-small" @click="$emit('add_subject', cell)">과목 선택</button>
+              <button class="button is-small" @click="$emit('add_bunban', cell)">분반만 선택</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <div class="field is-grouped is-grouped-centered">
       <button class="button is-small" @click="previous_page" :disabled="index == 0">이전 페이지</button>
-      {{index+1}} / {{max_page+1}}
+      <span class="button is-small" disabled>{{index+1}} / {{max_page+1}}</span>
       <button class="button is-small" @click="next_page" :disabled="index == max_page">다음 페이지</button>
     </div>
 
@@ -161,7 +95,7 @@ import 영어 from '../data/영어.json'
 import 교양필수 from '../data/교양필수.json'
 export default {
   name: 'subject-viewer',
-  props: ['add_subject', 'add_bunban', 'desired_subject'],
+  props: ['add_subject', 'add_bunban', 'desired_subject', 'category', 'search'],
   created (){
     this.list = [...전공, ...일반교양, ...핵심교양, ...영어, ...교양필수]
   },
@@ -206,9 +140,7 @@ export default {
     return {
       list: [],
       index: 0,
-      pagination: 12,
-      search: '',
-      category: '전체'
+      pagination: 12
     }
   }
 }
@@ -216,7 +148,5 @@ export default {
 <style>
 #subject-viewer{
   font-size: 0.8em;
-}
-table {
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -14,34 +14,84 @@
         </div>
       </div>
     </div>
-    <subject-viewer :desired_subject="희망과목" @add_subject="x=>희망과목_추가(x)" @add_bunban="x=>희망분반_추가(x)"></subject-viewer>
-    <div class="section">
-      <div class="container">
-        <!-- <div class="content control is-center">
-          <div class="field is-grouped is-grouped-multiline">
-            <div v-for="(item, i) in 희망과목" :key="i" class="control">
-              <div class="tags has-addons" v-if="item.code.indexOf('-')==-1">
-                <span class="tag is-warning">{{item.detail.subject}}</span>
-                <span class="tag is-primary">{{item.detail.credit}}</span>
-                <span class="tag is-danger">삭제</span>
-              </div>
-              <div class="tags has-addons" v-else>
-                <span class="tag is-warning">{{item.detail.subject}}</span>
-                <span class="tag is-primary">{{item.detail.credit}}</span>
-                <span class="tag is-info">{{item.detail.name_pf}}</span>
-                <span class="tag is-text">{{item.detail.sno.match(/(.*)-(.*)/)[2]}}분반</span>
-                <span class="tag is-danger">삭제</span>
+    <section class="section">
+      <div class="container control">
+        <div class="field is-horizontal">
+          <div class="field-label is-normal">
+            <label class="label">검색필터</label>
+          </div>
+          <div class="field-body">
+            <div class="field">
+              <p class="control is-expanded">
+                <input class="input" v-model="search" placeholder="과목이름/분류등을 입력해보세요">
+              </p>
+            </div>
+          </div>      
+        </div>
+        <div class="field is-horizontal">
+          <div class="field-label is-normal">
+            <label class="label">최소학점 / 최대학점</label>
+          </div>
+          <div class="field-body">
+            <div class="field">
+              <p class="control is-expanded">
+                <input class="input" v-model="minCredit" placeholder="최소학점">
+              </p>
+            </div>
+            <div class="field">
+              <p class="control is-expanded">
+                <input class="input" v-model="maxCredit" placeholder="최대학점">
+              </p>
+            </div>
+          </div>      
+        </div>
+        <div class="field is-horizontal">
+          <div class="field-label is-normal">
+            <label class="label">분류</label>
+          </div>
+          <div class="field-body">
+            <div class="field">
+              <div class="field is-grouped is-grouped-centered">
+                <label class="radio">
+                  <input type="radio" v-model="category" value="전체"> 전체
+                </label>
+                <label class="radio" >
+                  <input type="radio" v-model="category" value="전공선택"> 전공선택
+                </label>
+                <label class="radio" >
+                  <input type="radio" v-model="category" value="전공필수"> 전공필수
+                </label>
+                <label class="radio" >
+                  <input type="radio" v-model="category" value="핵심교양"> 핵심교양
+                </label>
+                <label class="radio" >
+                  <input type="radio" v-model="category" value="교양선택"> 교양선택
+                </label>
+                <label class="radio" >
+                  <input type="radio" v-model="category" value="교양필수"> 교양필수
+                </label>
               </div>
             </div>
-          </div>
-        </div> -->
+          </div>      
+        </div>
+      </div>  
+    </section>
+    <section class="section">
+      <!-- TODO: v-model 바인딩 -->
+      <subject-viewer :search="search" :category="category" :desired_subject="희망과목"
+       @add_subject="x=>희망과목_추가(x)" @add_bunban="x=>희망분반_추가(x)"
+       @delete_subject="x=>희망과목_삭제(x)" @delete_bunban="x=>희망분반_삭제(x)"
+       @toggle_subject="x=>필수과목_토글(x)"></subject-viewer>
+    </section>
+    <div class="section">
+      <div class="container">
         <button @click="getResult()" class="button is-centered">계산하기</button>
         <p class="help">계산에는 시간이 다소 걸릴 수 있습니다. (1분-3분)</p>
         <div v-if="result">
           <div v-if="result.length > 0">
-            <button @click="index = index==0?0:index-1">이전 페이지</button>
-            {{index}}
-            <button @click="index = index==result.length-1?result.length-1:index+1">다음 페이지</button>
+            <button class="button is-small" @click="index = index==0?0:index-1">이전 페이지</button>
+            <span class="button is-small">{{index}}</span>
+            <button class="button is-small" @click="index = index==result.length-1?result.length-1:index+1">다음 페이지</button>
           </div>
           <table>
             <thead>
@@ -94,10 +144,33 @@ export default {
       희망과목: [],
       필수과목: ['모바일응용소프트웨어설계', '디지털영상처리설계', '인공지능 응용시스템'],
       index: 0,
+      search: '',
+      maxCredit: 19, 
+      minCredit: 15,
+      category: '전체',
       isProgress: false
     }
   },
   methods: {
+    필수과목_토글(코드){
+      const index = this.희망과목.map(x=>x.code).indexOf(코드)
+      if(index != -1){
+        this.희망과목[index].important = !this.희망과목[index].important
+      }
+      
+    },
+    희망과목_삭제(과목코드){
+      const index = this.희망과목.map(x=>x.code).indexOf(과목코드)
+      if(index != -1){
+        this.희망과목.splice(index, 1)
+      }
+    },
+    희망분반_삭제(분반코드){
+      const index = this.희망과목.map(x=>x.code).indexOf(분반코드)
+      if(index != -1){
+        this.희망과목.splice(index, 1)
+      }
+    },
     희망과목_추가(과목) {
       // 같은 과목이 없을때
       const 과목코드 = 과목.sno.match(/(.*)-(.*)/)[1]
@@ -105,18 +178,21 @@ export default {
       if(!this.희망과목.map(x=>x.code).includes(과목코드)){
         this.희망과목.push({
           code: 과목코드,
-          detail: 과목
+          detail: 과목,
+          important: true
         })
       }
     },
     희망분반_추가(분반){
+      const 과목코드 = 분반.sno.match(/(.*)-(.*)/)[1]
       const 분반코드 = 분반.sno
       console.log(`분반코드 : ${분반코드}`)
       // 같은 분반 && 과목이 없을때 
-      if(!this.희망과목.map(x=>x.code).includes(분반코드)){
+      if(!this.희망과목.map(x=>x.code).includes(분반코드) && !this.희망과목.map(x=>x.code).includes(과목코드) ){
         this.희망과목.push({
           code: 분반코드,
-          detail: 분반
+          detail: 분반,
+          important: true
         })
       }
     },
