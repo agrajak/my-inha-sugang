f7fbaf0980d1222730c0db8c17cc334b692e67fc
Agrajak <korbots@gmail.com>
2019-02-21 19:23:01 +0900

    이제 과목.json 파일을 불러오지 않는다

--- init.js
+++ init.js
@@ -2,7 +2,6 @@ const { getTimeTable, getDeptCode } = require('inha-timetable')
 const fs = require('fs-extra')
 async function init () {
   const 과들 = ['정보통신공학과', '컴퓨터공학과']
-  const result = []
   // 일반교양, 영어, 핵심교양
   await Promise.all([
     getTimeTable(getDeptCode('정보통신공학과'), '일반교양', true, 'data/일반교양.json'),
@@ -10,23 +9,24 @@ async function init () {
     getTimeTable(getDeptCode('정보통신공학과'), '핵심교양', true, 'data/핵심교양.json')
   ])
   // 교양필수  
-  try{
-    const 교양필수 = []
-    for(let 과 of 과들){
-      교양필수.concat(await getTimeTable(getDeptCode(과), '교양필수', true))
-    }
-    await fs.writeFile('data/교양필수.json', 교양필수)
-  
-    const 전공 = []
-  
-    for(let 과 of 과들){
-      전공.concat(await getTimeTable(getDeptCode(과), '교양필수', true))
-    }
-    await fs.writeFile('data/전공.json', 전공)
+  let  교양필수 = []
+  for(let 과 of 과들){
+    교양필수 = [...교양필수, ...await getTimeTable(getDeptCode(과), '교양필수', true)]
   }
-  catch(e){
-    console.log('에러발생!')
-    console.log(e)
+  await fs.writeFile('data/교양필수.json', JSON.stringify(교양필수, null, 2))
+
+  let 전공 = []
+
+  for(let 과 of 과들){
+    전공 = [...전공, ...await getTimeTable(getDeptCode(과), '전공', true)]
   }
+  await fs.writeFile('data/전공.json', JSON.stringify(전공, null, 2))
 }
-init()
+init().then(()=>{
+  process.exit(0)
+})
+.catch((e)=>{
+  console.log('에러발생!')
+  console.log(e)
+  process.exit(1)
+})
